74f3cfd5950bb4682f628a36401e4b64dfa3587a
Allie <allisonruthrosenheim@gmail.com>
2026-03-01 13:14:11 -0600

    add main.cpp to project files

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//  ________________________
+// |    Allison Rosenheim   |
+// |    CS-210              |
+// |    02/14/26            |
+//  ------------------------
+
+#include "UserMenuAndInput.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	while (displayMenuAndGetInput()) {
+		cout << endl;
+	}
+
+	return 0;
+}
